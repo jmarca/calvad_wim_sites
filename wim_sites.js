1fed9d98ff95475cd7f36d23deefb40af72af126
James Marca <jmarca@translab.its.uci.edu>
2014-05-09 14:22:35 -0700

    trying out a hackity
    add get wim need plotting function
    
    retarded hack to graft it on to module.exports thing

--- wim_sites.js
+++ wim_sites.js
@@ -17,4 +17,22 @@ function get_wim_need_imputing(opts,cb){
     return null
 }
 
+// pass in year, config_file (optional) in opts object
+function get_wim_need_plotting(opts,cb){
+    var year = +opts.year
+    viewer({'view':'_design/wim/_view/plot_status'
+           ,'startkey':[year,"nothing" ]
+           ,'endkey':[year,"nothing",{}]
+           ,'reduce':false
+           ,'config_file':opts.config_file
+           }
+          ,function(err,docs){
+               if(err) throw new Error('oops')
+               cb(null,docs)
+               return null
+           })
+    return null
+}
+
 module.exports=get_wim_need_imputing
+module.exports.get_wim_need_plotting=get_wim_need_plotting
