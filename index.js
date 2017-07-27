b3d28ff9753baec159ee429074d2ad2e4c1b5579
James E. Marca <james@activimetrics.com>
2017-07-26 18:15:04 -0700

    wait.  stop.  reverse that
    redid the parser, output, etc to make more like site list for wim sites

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var sites = require('./lib/sitelist.js');
+var tams_sitelist = require('./lib/tams_sitelist.js');
 var wim_sites = require('./lib/wim_sites.js')
 var tams_sites = require('./lib/tams_sites.js')
 
@@ -12,6 +13,7 @@ module.exports.get_wim_merged=wim_sites.get_wim_merged
 
 module.exports.get_tams_imputed_status=tams_sites.get_tams_imputed_status
 module.exports.get_tams_need_imputing=tams_sites.get_tams_need_imputing
+module.exports.tams_sitelist =  tams_sitelist
 // module.exports.get_tams_need_plotting=tams_sites.get_tams_need_plotting
 // module.exports.get_tams_need_pairing=tams_sites.get_tams_need_pairing
 // module.exports.get_tams_merged=tams_sites.get_tams_merged
